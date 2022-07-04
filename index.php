96a98928c1def7cdc6527726d774e5f8e5558f1b
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-07-04 20:53:41 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<title>Yusif Ahmedov PHP</title>
+<h1>Yusif Ahmedov PHP</h1>
+<p>
+<?php 
+    $x = hash('sha256', 'Yusif Ahmedov');
+    echo "The SHA256 hash of \"Yusif Ahmedov\" is $x"
+?>
+<pre>
+    ASCII ART:
+    
+  __   __ 
+  \ \ / / 
+   \ V /  
+  U_|"|_u 
+    |_|   
+.-,//|(_  
+ \_) (__) 
+
+</pre>
+</p>
